fd4e08b535a54b18ac1032abe19f034b58ce4fe9
Ronatos <alexander.l.perez@outlook.com>
2021-11-20 11:04:21 -0500

    finished starting_area_4

--- src/starting_area_4.rs
+++ src/starting_area_4.rs
@@ -100,6 +100,22 @@ pub fn new() -> Grid {
             wall.clone(),wall.clone(),wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(),
             wall.clone(),wall.clone(),wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone()
         ]);
+
+        starting_area4 = place_door(starting_area4, Wall::East, 0, 1);
+        starting_area4 = place_door(starting_area4, Wall::West, 18, 19);
+        if dice::roll(12) <= 2 { // This will be a 5ft wide passage
+            starting_area4 = generate_5ft_passage_ns(starting_area4, 0, 1);
+        }
+        else { // This will be a 10ft wide passage
+            starting_area4 = generate_10ft_passage_ns(starting_area4, 0, 1);
+        }
+        if dice::roll(12) <= 2 { // This will be a 5ft wide passage
+            starting_area4 = generate_5ft_passage_ns(starting_area4, 6, 7);
+        }
+        else { // This will be a 10ft wide passage
+            starting_area4 = generate_10ft_passage_ns(starting_area4, 6, 7);
+        }
+        starting_area4
     }
     else {
         // Let's build a vertical starting area 4
@@ -125,40 +141,23 @@ pub fn new() -> Grid {
             wall.clone(),wall.clone(),wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(),
             wall.clone(),wall.clone(),wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone(), wall.clone()
         ]);
-    }
 
-    // Repeat exactly 3 times.
-    // 1. Select a wall from the remaining empty walls at random.
-    // 2. Build a door
-    // 3. Remove whichever wall is selected from the list of available walls.
-
-    let mut exits_to_build = 3;
-    let mut empty_walls = vec![Wall::North, Wall::South, Wall::East, Wall::West];
-    let mut rng = rand::thread_rng();
-    while exits_to_build > 0 {
-        let wall_index = rng.gen_range(0..empty_walls.len());
-        let wall_selection = &empty_walls[wall_index];
-
-        match wall_selection {
-            Wall::North => {
-                starting_area3 = place_door(starting_area3, Wall::North, 0, 1);
-            },
-            Wall::South => {
-                starting_area3 = place_door(starting_area3, Wall::South, 10, 11);
-            },
-            Wall::East => {
-                starting_area3 = place_door(starting_area3, Wall::East, 0, 1);
-            },
-            Wall::West => {
-                starting_area3 = place_door(starting_area3, Wall::West, 10, 11);
-            }
+        starting_area4 = place_door(starting_area4, Wall::North, 0, 1);
+        starting_area4 = place_door(starting_area4, Wall::South, 18, 19);
+        if dice::roll(12) <= 2 { // This will be a 5ft wide passage
+            starting_area4 = generate_5ft_passage_ew(starting_area4, 0, 1);
         }
-
-        empty_walls.remove(wall_index);
-        exits_to_build = exits_to_build - 1;
+        else { // This will be a 10ft wide passage
+            starting_area4 = generate_10ft_passage_ew(starting_area4, 0, 1);
+        }
+        if dice::roll(12) <= 2 { // This will be a 5ft wide passage
+            starting_area4 = generate_5ft_passage_ew(starting_area4, 6, 7);
+        }
+        else { // This will be a 10ft wide passage
+            starting_area4 = generate_10ft_passage_ew(starting_area4, 6, 7);
+        }
+        starting_area4
     }
-
-    starting_area3
 }
 
 enum Wall {
@@ -168,6 +167,50 @@ enum Wall {
     West
 }
 
+fn generate_5ft_passage_ns(mut starting_area: Grid, top_row: usize, bottom_row: usize) -> Grid {
+    let mut rng = rand::thread_rng();
+    let floor = Node::Tile(Tile {kind: TileKind::Floor, icon: TileIcon::Floor});
+
+    let column = rng.gen_range(2..6);
+    starting_area.nodes[column + (starting_area.columns * top_row)] = floor.clone();
+    starting_area.nodes[column + (starting_area.columns * bottom_row)] = floor.clone();
+    starting_area
+}
+
+fn generate_5ft_passage_ew(mut starting_area: Grid, left_column: usize, right_column: usize) -> Grid {
+    let mut rng = rand::thread_rng();
+    let floor = Node::Tile(Tile {kind: TileKind::Floor, icon: TileIcon::Floor});
+
+    let row = rng.gen_range(2..6);
+    starting_area.nodes[(row * starting_area.columns) + left_column] = floor.clone();
+    starting_area.nodes[(row * starting_area.columns) + right_column] = floor.clone();
+    starting_area
+}
+
+fn generate_10ft_passage_ns(mut starting_area: Grid, top_row: usize, bottom_row: usize) -> Grid {
+    let mut rng = rand::thread_rng();
+    let floor = Node::Tile(Tile {kind: TileKind::Floor, icon: TileIcon::Floor});
+
+    let left_column = rng.gen_range(2..5);
+    starting_area.nodes[left_column + (starting_area.columns * top_row)] = floor.clone();
+    starting_area.nodes[left_column + (starting_area.columns * bottom_row)] = floor.clone();
+    starting_area.nodes[(left_column + 1) + (starting_area.columns * top_row)] = floor.clone();
+    starting_area.nodes[(left_column + 1) + (starting_area.columns * bottom_row)] = floor.clone();
+    starting_area
+}
+
+fn generate_10ft_passage_ew(mut starting_area: Grid, left_column: usize, right_column: usize) -> Grid {
+    let mut rng = rand::thread_rng();
+    let floor = Node::Tile(Tile {kind: TileKind::Floor, icon: TileIcon::Floor});
+
+    let top_row = rng.gen_range(2..5);
+    starting_area.nodes[(top_row * starting_area.columns) + left_column] = floor.clone();
+    starting_area.nodes[(top_row * starting_area.columns) + right_column] = floor.clone();
+    starting_area.nodes[((top_row + 1) * starting_area.columns) + left_column] = floor.clone();
+    starting_area.nodes[((top_row + 1) * starting_area.columns) + right_column] = floor.clone();
+    starting_area
+}
+
 fn place_door(mut starting_area: Grid, wall: Wall, lower_coordinate: usize, higher_coordinate: usize) -> Grid {
     let mut rng = rand::thread_rng();
     let door = Node::Tile(Tile {kind: TileKind::Door, icon: TileIcon::Door});
@@ -175,28 +218,28 @@ fn place_door(mut starting_area: Grid, wall: Wall, lower_coordinate: usize, high
 
     match wall {
         Wall::North => {
-            let column = rng.gen_range(2..10);
+            let column = rng.gen_range(2..6);
             let top_row = lower_coordinate;
             let bottom_row = higher_coordinate;
             starting_area.nodes[column + (starting_area.columns * top_row)] = floor.clone();
             starting_area.nodes[column + (starting_area.columns * bottom_row)] = door.clone();
         },
         Wall::South => {
-            let column = rng.gen_range(2..10);
+            let column = rng.gen_range(2..6);
             let top_row = lower_coordinate;
             let bottom_row = higher_coordinate;
             starting_area.nodes[column + (starting_area.columns * top_row)] = door.clone();
             starting_area.nodes[column + (starting_area.columns * bottom_row)] = floor.clone();
         },
         Wall::East => {
-            let row = rng.gen_range(2..10);
+            let row = rng.gen_range(2..6);
             let left_column = lower_coordinate;
             let right_column = higher_coordinate;
             starting_area.nodes[(row * starting_area.columns) + left_column] = floor.clone();
             starting_area.nodes[(row * starting_area.columns) + right_column] = door.clone();
         },
         Wall::West => {
-            let row = rng.gen_range(2..10);
+            let row = rng.gen_range(2..6);
             let left_column = lower_coordinate;
             let right_column = higher_coordinate;
             starting_area.nodes[(row * starting_area.columns) + left_column] = door.clone();
