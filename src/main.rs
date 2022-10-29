4384a385e8f15656afa2f3908123f9f676ae10e5
Alexander Perez <n01413497@unf.edu>
2022-10-28 23:05:10 -0400

    reformatted map layout

--- src/main.rs
+++ src/main.rs
@@ -33,23 +33,9 @@ fn main() {
     let room11 = room::starting_area_9::new();
 
     let map = grid::Grid::new(4, vec![
-        // Row 1
-        Node::Room(room0),
-        Node::Room(room1),
-        Node::Room(room2),
-        Node::Room(room3),
-
-        // Row 2
-        Node::Room(room4),
-        Node::Room(room5),
-        Node::Room(room6),
-        Node::Room(room7),
-
-        // Row 3
-        Node::Room(room8),
-        Node::Room(room9),
-        Node::Room(room10),
-        Node::Room(room11),
+        Node::Room(room0), Node::Room(room1), Node::Room(room2), Node::Room(room3),
+        Node::Room(room4), Node::Room(room5), Node::Room(room6), Node::Room(room7),
+        Node::Room(room8), Node::Room(room9), Node::Room(room10),Node::Room(room11),
     ]);
 
     println!("{}{}{}{}{}{}{}{}{}{}{}{}", map.nodes[0], map.nodes[1], map.nodes[2], map.nodes[3], map.nodes[4], map.nodes[5], map.nodes[6], map.nodes[7], map.nodes[8], map.nodes[9], map.nodes[10], map.nodes[11]);
